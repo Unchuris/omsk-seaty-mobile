e56efda278e2c9530595d4a5f53b4cebc4bcbc6b
vlad.unchuris <vlad.unchuris@gmail.com>
2020-08-29 04:24:36 +0600

    change small bench designer

--- lib/ui/widgets/route.dart
+++ lib/ui/widgets/route.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../../app_localizations.dart';
@@ -8,7 +9,8 @@ class RouteButton extends StatefulWidget {
   final double lat;
   final double lon;
 
-  const RouteButton({Key key, @required this.lat, @required this.lon}) : super(key: key);
+  const RouteButton({Key key, @required this.lat, @required this.lon})
+      : super(key: key);
 
   @override
   _RouteButtonState createState() => _RouteButtonState();
@@ -25,18 +27,45 @@ class _RouteButtonState extends State<RouteButton> {
         style: Theme.of(context).textTheme.caption,
       ),
       fillColor: Theme.of(context).accentColor,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(4.0)),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4.0)),
     );
   }
+}
 
-  _launchURL(double lat, double lon) async {
-    var url =
-        'https://www.google.ru/maps/dir/?api=1&destination=$lat,$lon&travelmode=walking';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+class RouteIcon extends StatefulWidget {
+  final double lat;
+  final double lon;
+
+  const RouteIcon({Key key, @required this.lat, @required this.lon})
+      : super(key: key);
+
+  @override
+  _RouteIconState createState() => _RouteIconState();
+}
+
+class _RouteIconState extends State<RouteIcon> {
+  @override
+  Widget build(BuildContext context) {
+    return ClipOval(
+        child: Material(
+            color: Colors.white, // button color
+            child: InkWell(
+                child: Padding(
+                    padding: EdgeInsets.all(5),
+                    child: SizedBox(
+                        width: 24,
+                        height: 24,
+                        child: SvgPicture.asset("assets/road.svg"))),
+                onTap: () => _launchURL(widget.lat, widget.lon))));
+  }
+}
+
+_launchURL(double lat, double lon) async {
+  var url =
+      'https://www.google.ru/maps/dir/?api=1&destination=$lat,$lon&travelmode=walking';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
